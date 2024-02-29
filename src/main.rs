3c4414ab95a724b306aa4b7760dcaf563be63bd2
Simon Cornelius <simonjoecornelius@gmail.com>
2024-02-29 12:02:51 +0100

    wgpu example modified to fit the first learn-wgpu chapter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod app;
 // mod dns;
 // mod http;
+use winit::event_loop::EventLoop;
 
 fn main() {
-    let app = app::App::new();
-    app.run();
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    #[allow(unused_mut)]
+    let mut builder = winit::window::WindowBuilder::new();
+    let window = builder.build(&event_loop).unwrap();
+
+    futures::executor::block_on(app::App::run(event_loop, window));
     // let args: Vec<String> = std::env::args().collect();
     // if args.len() != 2 {
     //     panic!("Usage: {} <address>", args[0]);
