f8589f9d03a86bf29078e7af6e7b9b4350ce682f
Simon Cornelius <simonjoecornelius@gmail.com>
2024-02-29 08:19:02 +0100

    initial window creation
    Note: I would've liked to keep the depencies as low as possible and now
    there's 108 of them..
    I'm not too worried about the transitive depencies and it just makes the
    crate count go up, but I would've liked to implement a logger like
    env_logger myself. It might've not been a lot of work or very
    interesting but I guess that's one less thing to worry about now.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-mod dns;
-mod http;
+mod app;
+// mod dns;
+// mod http;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        panic!("Usage: {} <address>", args[0]);
-    }
-    let address = &args[1];
+    let app = app::App::new();
+    app.run();
+    // let args: Vec<String> = std::env::args().collect();
+    // if args.len() != 2 {
+    //     panic!("Usage: {} <address>", args[0]);
+    // }
+    // let address = &args[1];
 
-    println!("Opening: {}", address);
+    // println!("Opening: {}", address);
 
-    let _content = http::request_content(address).unwrap();
+    // let _content = http::request_content(address).unwrap();
     // println!("Content: {}", content.body.unwrap());
 }
