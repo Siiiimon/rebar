ce6494ef2e62f5a54709bfdeb98ab3128958cb6e
Simon Cornelius <simonjoecornelius@gmail.com>
2024-03-02 21:09:52 +0100

    remove commented out code for merge

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 mod app;
-// mod dns;
-// mod http;
+mod dns;
+mod http;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("Usage: {} <address>", args[0]);
+    }
+    let address = &args[1];
+    println!("Opening: {}", address);
+
     let mut application = app::App::init();
+    let app_future = application.run();
 
-    futures::executor::block_on(application.run());
-    // let args: Vec<String> = std::env::args().collect();
-    // if args.len() != 2 {
-    //     panic!("Usage: {} <address>", args[0]);
-    // }
-    // let address = &args[1];
+    let content = http::request_content(address).unwrap();
+    println!("Content: {}", content.body.unwrap());
 
-    // println!("Opening: {}", address);
-
-    // let _content = http::request_content(address).unwrap();
-    // println!("Content: {}", content.body.unwrap());
+    futures::executor::block_on(app_future);
 }
